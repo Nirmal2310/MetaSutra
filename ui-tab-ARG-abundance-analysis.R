dfdf542e302d6ae193ecac44cff37cf7489e74cd
Nirmal2310 <62856422+Nirmal2310@users.noreply.github.com>
2023-09-26 18:46:49 +0530

    Update ui-tab-ARG-abundance-analysis.R

--- ui-tab-ARG-abundance-analysis.R
+++ ui-tab-ARG-abundance-analysis.R
@@ -5,13 +5,13 @@ tabPanel(
                          tabPanel(title = "Control Richness",
                                   downloadButton(outputId = "control_download_circular_richness_plot",
                                                  label = "Download Control Richness Plot (PNG)"),
-                                  plotOutput("plot_control_circular_richness_plot", height = "100%")
+                                  plotOutput("plot_control_circular_richness_plot", height = "auto", width = "auto")
                                     
                                   ),
                          tabPanel(title = "Case Richness",
                                   downloadButton(outputId = "case_download_circular_richness_plot",
                                                  label = "Download Case Richness Plot(PNG)"),
-                                  plotOutput("plot_case_circular_richness_plot", height = "100%")
+                                  plotOutput("plot_case_circular_richness_plot", height = "auto", width = "auto")
                                 )
                        )
                        ),
@@ -20,13 +20,13 @@ tabPanel(
                                    tabPanel(title = "Control Abundance",
                                             downloadButton(outputId = "download_control_abundance_heatmap",
                                                            label = "Download Control Abundance HeatMap (PNG)"),
-                                            plotOutput("plot_control_abundance_heatmap", height = "150%", width = "100%")
+                                            plotOutput("plot_control_abundance_heatmap", height = "auto", width = "auto")
                                               
                                             ),
                                    tabPanel(title = "Case Abundance",
                                             downloadButton(outputId = "download_case_abundance_heatmap",
                                                            label = "Download Case Abundance HeatMap (PNG)"),
-                                            plotOutput("plot_case_abundance_heatmap", height = "150%", width = "100%")
+                                            plotOutput("plot_case_abundance_heatmap", height = "auto", width = "auto")
                                             )
                                    )
                        )
