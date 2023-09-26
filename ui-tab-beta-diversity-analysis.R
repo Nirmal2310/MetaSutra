3aa95d37984805c4514fb2ae37dfa4706b30adae
Nirmal2310 <62856422+Nirmal2310@users.noreply.github.com>
2023-09-26 18:44:49 +0530

    Update ui-tab-beta-diversity-analysis.R

--- ui-tab-beta-diversity-analysis.R
+++ ui-tab-beta-diversity-analysis.R
@@ -4,9 +4,9 @@ tabPanel(
               tabPanel(title = "Heat Map",
                        downloadButton(outputId = "download_heatmap",
                                       label = "Download Heat Map (PNG)"),
-                       plotOutput("plot_heatmap")),
+                       plotOutput("plot_heatmap", height = "100%", width = "100%")),
               tabPanel(title = "PCA Plot",
                        downloadButton(outputId = "download_pca",
                                       label = "Download PCA Plot (PNG)"),
-                       plotOutput("plot_pca")))
+                       plotOutput("plot_pca", height = "100%", width = "100%")))
 )
