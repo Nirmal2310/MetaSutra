6a3919530ad0a29aad8469b3bef61cd3165df3db
Nirmal2310 <62856422+Nirmal2310@users.noreply.github.com>
2024-02-18 20:19:04 +0530

    Update server-alpha-diversity.R

--- server-alpha-diversity.R
+++ server-alpha-diversity.R
@@ -6,16 +6,24 @@ alpha_diversity_analysis <- reactive({
         diversity_data <- data %>% group_by(Sample_Id,ARO_term) %>%
           summarise(Counts = sum(Normalized_counts)) %>%
           mutate(Abundance = Counts/sum(Counts)*100)
+        
         diversity_data <- inner_join(diversity_data, sample_metadata, by="Sample_Id")
+        
         diversity_data <- diversity_data %>% select(Sample_Id, Group, everything())
+        
         diversity_data <- diversity_data %>% group_by(Sample_Id) %>% summarise(Shannon = diversity(Abundance, index = "shannon"),
-        Simpson = diversity(Abundance, index = "simpson"))
+                                                                               Simpson = diversity(Abundance, index = "simpson"))
+        
         diversity_data <- inner_join(diversity_data, sample_metadata, by="Sample_Id")
+        
         diversity_data <- diversity_data %>% select(Sample_Id, Group, everything())
+        
         diversity_data$Group <- factor(diversity_data$Group,
                                 levels = c("Control", "Case"))
+        
         diversity_data <- pivot_longer(diversity_data, cols = c(Shannon, Simpson),
                                         names_to = "Diversity", values_to = "Value")
+        
         alpha_div_p <- compare_means(Value ~ Group, data = diversity_data,
                                      method = "wilcox", p.adjust.method = "holm",
                                      group.by = "Diversity")
@@ -66,19 +74,26 @@ alpha_diversity_analysis <- reactive({
         
         return(diversity_facet)
     }
+    
     fun_abundance_plot <- function(data)
     {
         abundance_data <- data %>% 
           group_by(Sample_Id, ARO_term) %>%
           summarise(Counts = sum(Normalized_counts))
+        
         abundance_data <- inner_join(abundance_data, sample_metadata,
                                     by = "Sample_Id")
+        
         abundance_data <- abundance_data %>% select(Sample_Id, Group, everything())
+        
         abundance_data$Group <- factor(abundance_data$Group,
                                     levels = c("Control", "Case"))
+        
         abundance_data <- subset(abundance_data, select = -Sample_Id)
+        
         abundance_p_value <- compare_means(Counts~Group, data = abundance_data, method = "wilcox",
                                            p.adjust.method = "holm")
+        
         abundance_plot <- ggplot(abundance_data, aes(x = Group, y = log2(Counts), color = Group)) +
           geom_boxplot() +
           stat_pvalue_manual(abundance_p_value, y.position = max(log2(abundance_data$Counts)) + 0.5,
@@ -97,6 +112,7 @@ alpha_diversity_analysis <- reactive({
             legend.text = element_text(colour = "black", size = 10, face = "bold"),
             plot.title = element_text(hjust = 0.5, size = rel(2))
           )
+        
         association_data <- data %>% group_by(Sample_Id, ARO_term) %>% 
           summarise(Summed_Counts = sum(Counts), length = max(ARG_length)/1000)
         
@@ -142,9 +158,11 @@ alpha_diversity_analysis <- reactive({
         
         return(abundance_facet)
     }
+    
     return(list(diversity_plot = fun_diversity_plot(data),
         abundance_plot = fun_abundance_plot(data)))
 })
+
 observeEvent(input$upload_data, {
     plots_data <- alpha_diversity_analysis()
     output$plot_alpha_diversity <- renderPlot({
